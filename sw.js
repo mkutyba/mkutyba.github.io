48e8fc49df74a75e21c799d527dcff6bdfd88a47
Mateusz Kutyba <mateusz.kutyba@gmail.com>
2017-08-18 13:57:53 +0200

    Introduce PWA (Progressive Web App) to cache content in local storage

---
+++ sw.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = 'app-cache';
+
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/blog.html',
+  '/ecommerce.html',
+  '/get-noticed.html',
+  '/blog/i-started-blogging-again.html',
+  '/blog/consumer-forms-project-description.html',
+  '/blog/testing-testing-testing.html',
+  '/blog/paczka-w-ruchu-modul-do-magento-1-9.html',
+  '/assets/images/bg-no-credits.jpg',
+  '/assets/images/mateusz-kutyba.png',
+  '/assets/fonts/fontawesome-webfont-4.6.3.eot',
+  '/assets/fonts/fontawesome-webfont-4.6.3.svg',
+  '/assets/fonts/fontawesome-webfont-4.6.3.ttf',
+  '/assets/fonts/fontawesome-webfont-4.6.3.woff',
+  '/assets/fonts/fontawesome-webfont-4.6.3.woff2',
+  '/assets/images/103084-OM8M26-164.jpg',
+  '/assets/images/magento-paczka-w-ruchu.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => {
+    return cache.addAll(urlsToCache);
+  }));
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(caches.match(event.request).then((response) => {
+    return response ? response : fetch(event.request);
+  }));
+});
